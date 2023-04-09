4594e90a4c05dedbaf37366797b40ce8548cccf0
Maria <stepanovamasha70@gmail.com>
2023-04-09 16:44:33 +0300

    add task 2

--- src/task_2/FizzBuzzPrinter.java
+++ src/task_2/FizzBuzzPrinter.java
@@ -1,74 +1,47 @@
 package task_2;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.*;
 
-public class FizzBuzzPrinter extends Thread{
+public class FizzBuzzPrinter{
     private int n;
-    private BlockingQueue<String> queue; // потокобезпечна черга
+    private CopyOnWriteArrayList<String> list; // потокобезпечна черга
     ExecutorService executor;
 
     public FizzBuzzPrinter(int n) {
         this.n = n;
-        this.queue =  new LinkedBlockingQueue<>();
+        this.list =  new CopyOnWriteArrayList<>();
         this.executor = Executors.newFixedThreadPool(4); // створили пул з 4 потоків
     }
 
     public void fizz(){
         for(int i = 1; i <= n; i++){
             if(i%3 == 0 && i%5 != 0){
-                try{
-                    queue.put("fizz");
-                }catch(InterruptedException ex){
-                    ex.printStackTrace();
-                }
+                list.add("fizz");
             }
         }
     }
     public void buzz(){
         for(int i = 1; i <= n; i++){
             if(i%5 == 0 && i%3 != 0){
-                try{
-                    queue.put("buzz");
-                }catch(InterruptedException ex){
-                    ex.printStackTrace();
-                }
+                list.add("buzz");
             }
         }
     }
     public void fizzbuzz(){
         for(int i = 1; i <= n; i++){
             if(i%15 == 0){
-                try{
-                    queue.put("fizzbuzz");
-                }catch(InterruptedException ex){
-                    ex.printStackTrace();
-                }
+                list.add("fizzbuzz");
             }
         }
     }
     public void number(){
         for(int i = 1; i <= n; i++){
             if(i%5 != 0 && i%3 !=0){
-                try{
-                    String num = String.valueOf(i);
-                    queue.put(num);
-                }catch(InterruptedException ex){
-                    ex.printStackTrace();
-                }
+                list.add(Integer.toString(i));
             }
-            executor.shutdown(); // Виконання потоків завершено
         }
     }
     public void printQueue(){
-        while (!queue.isEmpty()) {
-            try {
-                System.out.print(queue.take() + " ");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        System.out.print(String.join(", ", list));
     }
 }
